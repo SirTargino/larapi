0509373e566da555371680da559708708f4d699b
João Targino <joaoetargino@gmail.com>
2024-08-05 12:35:13 -0300

    first commit

---
+++ src/main/java/com/laratelie/larapi/LarapiApplication.java
@@ -0,0 +1,13 @@
+package com.laratelie.larapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laratelie/larapi/LarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laratelie.larapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
