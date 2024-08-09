b8e9e067d203dffac3da7aa00b2c1f1995e8a51d
João Targino <joaoetargino@gmail.com>
2024-08-08 21:10:33 -0300

    feat: created order repository

--- src/main/java/com/laratelie/larapi/repositories/OrderRepository.java
+++ src/main/java/com/laratelie/larapi/repositories/OrderRepository.java
@@ -1,15 +1,14 @@
 package com.laratelie.larapi.repositories;
 
-import com.laratelie.larapi.models.Order;
+import com.laratelie.larapi.models.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-    @Query("SELECT e FROM Order e WHERE e.client = :client")
-    Order findByClient(@Param("client") String client);
+public interface OrderRepository extends JpaRepository<CustomerOrder, UUID> {
+    @Query("SELECT e FROM CustomerOrder e WHERE e.client = :client")
+    CustomerOrder findByClient(@Param("client") String client);
 
-    boolean existsByClient(String client);
 }
